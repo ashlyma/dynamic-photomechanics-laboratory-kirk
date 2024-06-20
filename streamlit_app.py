75a3bb81660537cb9ff9e7ae3fb62390a2eb9aa0
ashlyma <79590820+ashlyma@users.noreply.github.com>
2024-06-20 18:09:50 +0000

    Name, date, and facility drop-down commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,16 @@ import pandas as pd
 import streamlit as st
 import datetime
 
-st.title('Welcome Dynamic Photomechanics Laboratory Kirk')
+#Displays the first tittle of the page
+st.header("Dynamic Photomechanics Laboratory")
+
+#Text box that will acquire the users name (Potentially changed to drop-down with usernames)
+username_textbox = st.text_input("Enter your name:", placeholder = "Enter your first and last name...")
+
+#Package that takes the date that the experiment ran
+Date_form = st.date_input("Date input:", value=None)
+
+#Facility type select-box 
+facility_type = st.selectbox("Select facility:",[''])
+#The button used to create the directory
+st.button("Create")
