16fcc7c04e1abf15f909e559b92f9953d1f948dd
ashlyma <79590820+ashlyma@users.noreply.github.com>
2024-06-21 18:49:08 +0000

    Finished form questions

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,17 +4,23 @@ import pandas as pd
 import streamlit as st
 import datetime
 
+# Function that displays a text box if "Other" is selected for multiple questions
+def other_textbox(selected_option, label, key):
+    if selected_option == "Other":
+        selected_option = st.text_input("Other:", placeholder= "Please enter other...")
+    return None
 #Displays the first tittle of the page
 st.header("Dynamic Photomechanics Laboratory")
 
 #Text box that will acquire the users name (Potentially changed to drop-down with usernames)
-username_textbox = st.text_input("Enter your name:", placeholder = "Enter your first and last name...")
+username_textbox = st.text_input("Name:", placeholder = "Enter your first and last name...")
 
 #Package that takes the date that the experiment ran
-Date_form = st.date_input("Date input:", value=None)
+Date_form = st.date_input("Experiment run-date:", value=None)
 
 # Defines the main categories of facilities and their corresponding sub-options
-facility_options = {
+facility_options = { 
+    "Select": [],
     "Implosion Tank (aka 'Big' Tank)": ["Implosion", "UNDEX", "Other"],
     "Explosion Tank (aka 'Square' Tank)": ["UNDEX", "Other"],
     "Shock Tube (Gas)": ["AIREX", "Other"],
@@ -29,15 +35,27 @@ facility_options = {
 }
 
 # First drop-down for selecting the main facility type
-facility_type_main_option = st.selectbox("Select Facility Type", list(facility_options.keys()))
+facility_type_main_option = st.selectbox("Facility Type:", list(facility_options.keys()))
 
-# Second drop-down for selecting the sub-option based on the main facility selection
+# Second drop-down for selecting the sub-option based on the main facility and calls other function to show the text box for "other"
 if facility_type_main_option:
-    facility_type_sub_option = st.selectbox(f"Select the facility sub-option for {facility_type_main_option}", facility_options[facility_type_main_option])
+    facility_type_sub_option = st.selectbox(f"Experiment type for {facility_type_main_option}:", facility_options[facility_type_main_option])
+    facility_other = other_textbox(facility_type_sub_option, "facility type", key="facility_other")
 
-# Updates the facility_type_sub_option drop-down to a text box if the "Other" option is selected 
-if facility_type_sub_option == "Other":
-    facility_type_sub_option = st.text_input("Enter facility type:", placeholder = "Please enter the facility used...")
+# Material type dropdown and calls other function to show the text box for "other"
+material_type_option = st.selectbox("Material type:",("Select","Metallic","Composite","Polymer","Ceramic","Other"))
+material_other = other_textbox(material_type_option, "material type", key="material_other")
+
+# Text box input for the load type
+input_load_type = st.text_input("Input (Load) Magnitude in SI Units:", placeholder = "Enter Magnitude")
+
+# load type dropdown and calls other function to show the text box for "other"
+load_type_dropdown = st.selectbox("Load Type:", ("Select","Force (kN)", "Pressure (MPa)", 
+    "Energy (J)", "Displacement (mm)", "Velocity (m/s)", "Acceleration (m/s^2)", "Other"))
+load_other = other_textbox(load_type_dropdown, "load type", key="load_other")
+
+# Trial number text box
+trial_input = st.number_input("Trial #:",value= None, placeholder= "Enter trial number..")
 
 #The button used to create the directory
 st.button("Create")
