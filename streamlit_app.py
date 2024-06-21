e88661d0623425a7e728ebaecbaf394685690662
ashlyma <79590820+ashlyma@users.noreply.github.com>
2024-06-21 19:37:49 +0000

    Added a new tab called "Search"

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,58 +4,62 @@ import pandas as pd
 import streamlit as st
 import datetime
 
-# Function that displays a text box if "Other" is selected for multiple questions
-def other_textbox(selected_option, label, key):
-    if selected_option == "Other":
-        selected_option = st.text_input("Other:", placeholder= "Please enter other...")
-    return None
-#Displays the first tittle of the page
-st.header("Dynamic Photomechanics Laboratory")
+tab1, tab2 = st.tabs(["Form", "Search"])
 
-#Text box that will acquire the users name (Potentially changed to drop-down with usernames)
-username_textbox = st.text_input("Name:", placeholder = "Enter your first and last name...")
+with tab1:
+    # Function that displays a text box if "Other" is selected for multiple questions
+    def other_textbox(selected_option, label, key):
+        if selected_option == "Other":
+            selected_option = st.text_input("Other:", placeholder= "Please enter other...")
+        return None
+    #Displays the first tittle of the page
+    st.header("Dynamic Photomechanics Laboratory")
 
-#Package that takes the date that the experiment ran
-Date_form = st.date_input("Experiment run-date:", value=None)
+    #Text box that will acquire the users name (Potentially changed to drop-down with usernames)
+    username_textbox = st.text_input("Name:", placeholder = "Enter your first and last name...")
 
-# Defines the main categories of facilities and their corresponding sub-options
-facility_options = { 
-    "Select": [],
-    "Implosion Tank (aka 'Big' Tank)": ["Implosion", "UNDEX", "Other"],
-    "Explosion Tank (aka 'Square' Tank)": ["UNDEX", "Other"],
-    "Shock Tube (Gas)": ["AIREX", "Other"],
-    "Hypersonic Shock Tube (Gas)": ["AIREX", "Other"],
-    "Shock Tube (Water)": ["Air-backed Plate Shock", "Submerged UNDEX", "Other"], 
-    "Flyer Plate Impactor (Gas Gun)": ["Spalling", "Ballistic", "Other"], 
-    "SHPB (Dynamic)": ["Compression", "Tension", "Shear","Fracture", "Other"],
-    "Drop Weight tower (Dynamic)": ["Plate Impact", "Three Point Bending", "Compression", "Other"],
-    "HITS (Dynamic)": ["Puncture","Compression", "Other"], 
-    "Dynamic Mechanical Analyzer (DMA)":["Three Point Bending", "Cantilever","Double Cantilever","Compression", "Tension", "Other"],
-    "UTM (Static)": ["Tension", "Compression", "Shear", "Three Point Bending","Four Point Bending" "Peel", "Other"],
-}
+    #Package that takes the date that the experiment ran
+    Date_form = st.date_input("Experiment run-date:", value=None)
 
-# First drop-down for selecting the main facility type
-facility_type_main_option = st.selectbox("Facility Type:", list(facility_options.keys()))
+    # Defines the main categories of facilities and their corresponding sub-options
+    facility_options = { 
+        "None": [],
+        "Implosion Tank (aka 'Big' Tank)": ["Implosion", "UNDEX", "Other"],
+        "Explosion Tank (aka 'Square' Tank)": ["UNDEX", "Other"],
+        "Shock Tube (Gas)": ["AIREX", "Other"],
+        "Hypersonic Shock Tube (Gas)": ["AIREX", "Other"],
+        "Shock Tube (Water)": ["Air-backed Plate Shock", "Submerged UNDEX", "Other"], 
+        "Flyer Plate Impactor (Gas Gun)": ["Spalling", "Ballistic", "Other"], 
+        "SHPB (Dynamic)": ["Compression", "Tension", "Shear","Fracture", "Other"],
+        "Drop Weight tower (Dynamic)": ["Plate Impact", "Three Point Bending", "Compression", "Other"],
+        "HITS (Dynamic)": ["Puncture","Compression", "Other"], 
+        "Dynamic Mechanical Analyzer (DMA)":["Three Point Bending", "Cantilever","Double Cantilever","Compression", "Tension", "Other"],
+        "UTM (Static)": ["Tension", "Compression", "Shear", "Three Point Bending","Four Point Bending" "Peel", "Other"],
+    }
+    # First drop-down for selecting the main facility type
+    facility_type_main_option = st.selectbox("Facility Type:", list(facility_options.keys()))
 
-# Second drop-down for selecting the sub-option based on the main facility and calls other function to show the text box for "other"
-if facility_type_main_option:
-    facility_type_sub_option = st.selectbox(f"Experiment type for {facility_type_main_option}:", facility_options[facility_type_main_option])
-    facility_other = other_textbox(facility_type_sub_option, "facility type", key="facility_other")
+    # Second drop-down for selecting the sub-option based on the main facility and calls other function to show the text box for "other"
+    if facility_type_main_option:
+        facility_type_sub_option = st.selectbox(f"Experiment type for {facility_type_main_option}:", facility_options[facility_type_main_option])
+        facility_other = other_textbox(facility_type_sub_option, "facility type", key="facility_other")
 
-# Material type dropdown and calls other function to show the text box for "other"
-material_type_option = st.selectbox("Material type:",("Select","Metallic","Composite","Polymer","Ceramic","Other"))
-material_other = other_textbox(material_type_option, "material type", key="material_other")
+    # Material type dropdown and calls other function to show the text box for "other"
+    material_type_option = st.selectbox("Material type:",("None","Metallic","Composite","Polymer","Ceramic","Other"))
+    material_other = other_textbox(material_type_option, "material type", key="material_other")
 
-# Text box input for the load type
-input_load_type = st.text_input("Input (Load) Magnitude in SI Units:", placeholder = "Enter Magnitude")
+    # Text box input for the load type
+    input_load_type = st.text_input("Input (Load) Magnitude in SI Units:", placeholder = "Enter Magnitude")
 
-# load type dropdown and calls other function to show the text box for "other"
-load_type_dropdown = st.selectbox("Load Type:", ("Select","Force (kN)", "Pressure (MPa)", 
-    "Energy (J)", "Displacement (mm)", "Velocity (m/s)", "Acceleration (m/s^2)", "Other"))
-load_other = other_textbox(load_type_dropdown, "load type", key="load_other")
+    # load type dropdown and calls other function to show the text box for "other"
+    load_type_dropdown = st.selectbox("Load Type:", ("None","Force (kN)", "Pressure (MPa)", 
+        "Energy (J)", "Displacement (mm)", "Velocity (m/s)", "Acceleration (m/s^2)", "Other"))
+    load_other = other_textbox(load_type_dropdown, "load type", key="load_other")
 
-# Trial number text box
-trial_input = st.number_input("Trial #:",value= None, placeholder= "Enter trial number..")
+    # Trial number text box
+    trial_input = st.number_input("Trial #:", min_value=1, step=1, placeholder="Enter trial number...")
 
-#The button used to create the directory
-st.button("Create")
+    #The button used to create the directory
+    st.write("Directory file name:")
+    st.write(username_textbox, "/", facility_type_main_option, "/",facility_type_sub_option, "/", "[", Date_form,"/", material_type_option,"/", "Trial:", trial_input, "]")
+    st.button("Create")
